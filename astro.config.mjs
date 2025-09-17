9d67fe77d6c4a7968b1858ef93d8144c10a58ebc
Matt O'Leary <matto2@users.noreply.github.com>
2025-09-16 20:17:38 -0700

    stripe updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-
+import netlify from '@astrojs/netlify';
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  output: 'hybrid',
+  adapter: netlify(),
   devToolbar: {
     enabled: false
   }
